fa61d240f070a40354158faae2ef2d5a57523abf
Spencer Foster <fosterjs@me.com>
2017-11-12 02:19:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Write your code in this file!
 const currentUser = 'Spencer Foster';
- 
+
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
